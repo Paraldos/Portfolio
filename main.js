5e980c3389fe0fbd0f350bc1b9be81efa208b453
Lil Cthulhu <hillbrecht.s@gmail.com>
2022-10-29 11:36:42 +0200

    responsive footer
    made the footer responsive

--- main.js
+++ main.js
@@ -2,13 +2,15 @@
 
 const MENUBTN = document.querySelector(".menu_btn");
 const NAVBAR = document.querySelector(".navbar");
+const FOOTER = document.querySelector(".footer");
 
 // opens and close navbar if screen is smaller then 850 px
 MENUBTN.addEventListener("click", () => {
   if (NAVBAR.classList.contains("open")) {
     NAVBAR.classList.remove("open");
+    FOOTER.classList.remove("open");
   } else {
     NAVBAR.classList.add("open");
+    FOOTER.classList.add("open");
   }
-  nav_Menu = !nav_Menu;
 });

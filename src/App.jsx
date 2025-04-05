7425c3e6d0819158d4a766736e206f9162b98138
Paraldos <hillbrecht.s@gmail.com>
2025-04-05 12:25:44 +0200

    added and refined about

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import About from "./components/about/About";
 import Footer from "./components/footer/Footer";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <main>
         <Hero />
+        <About />
       </main>
       <Footer />
     </>
---
+++ src/components/about/About.jsx
@@ -0,0 +1,37 @@
+import "./about.css";
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        My name is Sieghard, and I am a game and web developer. I started coding
+        back in 2019 out of curiosity but soon found myself enthralled by
+        creating my own games, websites, and web apps. This passion became my
+        new favorite hobby, and it drives me to get a little better with every
+        new project I start.
+      </p>
+      <p>Here are a few of the technologies I'm familiar with:</p>
+      <ul class="about__list">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>React</li>
+        <li>PHP</li>
+        <li>Composer</li>
+        <li>Git</li>
+        <li>NPM</li>
+        <li>AEM (Adobe)</li>
+        <li>NEOS CMS</li>
+      </ul>
+      <ul class="about__list">
+        <li>Godot</li>
+        <li>GDScript</li>
+        <li>Unreal Engine</li>
+        <li>Aseprite</li>
+      </ul>
+    </section>
+  );
+}
+
+export default About;

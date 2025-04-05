9e6a5df97232d7622efe9bd4f4f2c1b08b34e31e
Paraldos <hillbrecht.s@gmail.com>
2025-04-05 11:29:52 +0200

    hero + fonts

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,13 @@
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <section id="hero">
+      <p>Hello Friend, my name is</p>
+      <h1>Sieghard</h1>
+      <h3>I build games and things for the web</h3>
+    </section>
+  );
+}
+
+export default Hero;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/basics.css";
-import "./styles/variables.css";
+import "./styles/_import.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(

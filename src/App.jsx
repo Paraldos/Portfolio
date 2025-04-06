a024941732f458840a3819b94180c7af496421df
Paraldos <hillbrecht.s@gmail.com>
2025-04-06 20:14:15 +0200

    added game examples

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import About from "./components/about/About";
 import Resume from "./components/resume/Resume";
+import GameExamples from "./components/gameExamples/GameExamples";
 import Footer from "./components/footer/Footer";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Hero />
         <About />
         <Resume />
+        <GameExamples />
       </main>
       <Footer />
     </>
